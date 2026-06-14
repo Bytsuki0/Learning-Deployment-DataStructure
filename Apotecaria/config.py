d680ee4c1a6f8d3c7371b3080a86e988b452d2bc
Bytsuki0 <gustavoboss066@gmail.com>
2026-06-14 20:09:41 -0300

    dados

--- Apotecaria/config.py
+++ Apotecaria/config.py
@@ -1,17 +1,9 @@
 import os
 
 class Config:
-    if os.name == "posix":  # Linux/macOS
-        MYSQL_HOST = "127.0.0.1"
-        MYSQL_PORT = 3306
-        MYSQL_USER = "root"
-        MYSQL_PASSWORD = ""
-        MYSQL_DB = "clinica"
-        MYSQL_CURSORCLASS = "DictCursor"
-    else:  # Windows
-        MYSQL_HOST = "127.0.0.1"
-        MYSQL_PORT = 3307
-        MYSQL_USER = "root"
-        MYSQL_PASSWORD = ""
-        MYSQL_DB = "clinica"
-        MYSQL_CURSORCLASS = "DictCursor"
+    MYSQL_HOST = "127.0.0.1"
+    MYSQL_PORT = 3307
+    MYSQL_USER = "root"
+    MYSQL_PASSWORD = ""
+    MYSQL_DB = "clinica"
+    MYSQL_CURSORCLASS = "DictCursor"
